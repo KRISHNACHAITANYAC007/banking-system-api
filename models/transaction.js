3231b081fde985b486743ba632f0a8640a69b279
CHILLAPPAGARI KRISHNA CHAITANYA <ckrishnachaitanya007@gmail.com>
2026-03-11 19:45:25 +0530

    Add Transaction class to manage transactions

--- models/transaction.js
+++ models/transaction.js
@@ -1,1 +1,10 @@
+class Transaction {
+    constructor(accountId, type, amount) {
+        this.accountId = accountId
+        this.type = type
+        this.amount = amount
+        this.date = new Date()
+    }
+}
 
+module.exports = Transaction
