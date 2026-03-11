b97e3c8bfc8eb63091af1ff608071faed5b49176
CHILLAPPAGARI KRISHNA CHAITANYA <ckrishnachaitanya007@gmail.com>
2026-03-11 19:45:41 +0530

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -1,1 +1,19 @@
+const Account = require("../models/account")
 
+let accounts = []
+let idCounter = 1
+
+function createAccount(name) {
+    const account = new Account(idCounter++, name)
+    accounts.push(account)
+    return account
+}
+
+function getAccount(id) {
+    return accounts.find(acc => acc.id === id)
+}
+
+module.exports = {
+    createAccount,
+    getAccount
+}
