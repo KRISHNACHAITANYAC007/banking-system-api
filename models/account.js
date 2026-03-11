f15abf9218fe800bccc03e1915d91630a46d599f
CHILLAPPAGARI KRISHNA CHAITANYA <ckrishnachaitanya007@gmail.com>
2026-03-11 19:45:04 +0530

    Update account.js

--- models/account.js
+++ models/account.js
@@ -1,1 +1,20 @@
+class Account {
+    constructor(id, name, balance = 0) {
+        this.id = id
+        this.name = name
+        this.balance = balance
+    }
 
+    deposit(amount) {
+        this.balance += amount
+    }
+
+    withdraw(amount) {
+        if (amount > this.balance) {
+            throw new Error("Insufficient balance")
+        }
+        this.balance -= amount
+    }
+}
+
+module.exports = Account
