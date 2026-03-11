c18d912a728287c91cfc78aec482b140718dfc24
CHILLAPPAGARI KRISHNA CHAITANYA <ckrishnachaitanya007@gmail.com>
2026-03-11 19:45:56 +0530

    Update transaction.js

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,1 +1,44 @@
+const Transaction = require("../models/transaction")
+const { getAccount } = require("./account")
 
+let transactions = []
+
+function deposit(accountId, amount) {
+    const account = getAccount(accountId)
+
+    if (!account) {
+        throw new Error("Account not found")
+    }
+
+    account.deposit(amount)
+
+    const tx = new Transaction(accountId, "DEPOSIT", amount)
+    transactions.push(tx)
+
+    return tx
+}
+
+function withdraw(accountId, amount) {
+    const account = getAccount(accountId)
+
+    if (!account) {
+        throw new Error("Account not found")
+    }
+
+    account.withdraw(amount)
+
+    const tx = new Transaction(accountId, "WITHDRAW", amount)
+    transactions.push(tx)
+
+    return tx
+}
+
+function getTransactions(accountId) {
+    return transactions.filter(tx => tx.accountId === accountId)
+}
+
+module.exports = {
+    deposit,
+    withdraw,
+    getTransactions
+}
